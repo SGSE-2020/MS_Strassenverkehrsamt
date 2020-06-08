0ffb90f187396097f73c9757ad17b0d36965c652
dbene <dbene@users.noreply.github.com>
2020-06-08 13:19:14 +0200

    login form added

--- frontend/pages/account.vue
+++ frontend/pages/account.vue
@@ -1,7 +1,47 @@
 <template>
   <v-layout>
     <v-flex class="text-center">
-      <p>Melde dich mit deinem SmartCity Konto an</p>
+      <h1>Melde dich mit deinem SmartCity Konto an</h1>
+      <br />
+      <v-card width="500" class="mx-auto mt-5">
+        <v-card-title><h1 class="display-1">Login</h1></v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Benutzername"
+              prepend-icon="mdi-account-circle"
+            />
+            <v-text-field
+              :type="showPassword ? 'text' : 'password'"
+              prepend-icon="mdi-lock"
+              append-icon="mdi-eye-off"
+              label="Passwort"
+              @click:append="showPassword = !showPassword"
+            />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      showPassword: false
+    }
+  },
+  computed: {},
+  mounted() {},
+  methods: {
+    login() {
+      console.log('loginUser')
+    }
+  }
+}
+</script>
