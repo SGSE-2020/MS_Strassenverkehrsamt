6a5395f93693ad12dbeaee31d4598adbefb8790c
dbene <dbene@users.noreply.github.com>
2020-06-15 22:34:38 +0200

    logging update res response fix

--- backend/components/api_rest.js
+++ backend/components/api_rest.js
@@ -33,11 +33,11 @@ module.exports = function (config) {
             useUnifiedTopology: true
         })
         .then(client => {
-            const db = client.db('stva')
+            const db = client.db('stva');
+            db.collection("log").remove();
 
             app.get('/log/all', function (req, res) {
-                var query = {};
-                db.collection("log").find(query).toArray(function (err, result) {
+                db.collection("log").find({}).sort({timestamp: -1}).toArray(function (err, result) {
                     if (err) {
                         res.status(501).send({
                             message: "failure"
@@ -123,26 +123,26 @@ module.exports = function (config) {
                         grpcClient.verifyUser({
                                 token: req.headers.authorization
                             })
-                            .then(res => {
-                                console.log(res);
+                            .then(result => {
+                                console.log(result);
 
                                 res.type('application/json');
                                 res.status(200).send({
                                     somedata: ['Hallo', 'dies', 'sind', 'daten', 'aus', 'der', 'API'],
-                                    content: JSON.stringify(res)
+                                    content: JSON.stringify(result)
                                 });
-                                
-                                db.collection("log").insertOne({type: 'grpc-res', time: new Date().toISOString(), msg: res});
+
+                                db.collection("log").insertOne({type: 'grpc-res', timestamp: new Date().toISOString(), msg: result});
                             }).catch(err => {
                                 console.error(err)                                
-                                db.collection("log").insertOne({type: 'grpc-catch', time: new Date().toISOString(), msg: err});
+                                db.collection("log").insertOne({type: 'grpc-catch', timestamp: new Date().toISOString(), msg: err});
                                 res.status(500).send({
                                     position: "grpc catch",
                                     error: JSON.stringify(err)
                                 })
                             })
                     } catch (e) {
-                        db.collection("log").insertOne({type: 'grpc-catch-all', time: new Date().toISOString(), msg: e});
+                        db.collection("log").insertOne({type: 'grpc-catch-all', timestamp: new Date().toISOString(), msg: e});
                         res.status(500).send({
                             position: "catch",
                             error: JSON.stringify(e)
