c3cbb1310e537d257763e64700164ac139e9df12
dbene <dbene@users.noreply.github.com>
2020-06-29 18:44:22 +0200

    pwa upgrade

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -99,6 +99,35 @@ export default {
     }
   },
   /*
+   ** PWA Config
+   */
+  pwa: {
+    manifest: {
+      name: 'Straßenverkehrsamt SmartCity',
+      short_name: 'Strassenverkehrsamt',
+      lang: 'de',
+      background_color: "#5C6BC0",
+      description: "Die App für das Strassenverkehrsamt von SmartCity",
+      "icons": [{
+        "src": "ic_launcher_mdpi.png",
+        "sizes": "48x48",
+        "type": "image/png"
+      }, {
+        "src": "ic_launcher_hdpi.png",
+        "sizes": "72x72",
+        "type": "image/png"
+      }, {
+        "src": "ic_launcher_xxhdpi.png",
+        "sizes": "144x144",
+        "type": "image/png"
+      }, {
+        "src": "ic_launcher_xxxhdpi.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      }]
+    }
+  },
+  /*
    ** Build configuration
    */
   build: {
