f175b09936ad1bcaad3ee11268913116cb3457cd
dbene <dbene@users.noreply.github.com>
2020-07-01 19:48:33 +0200

    dependencies fix

--- backend/test/server.js
+++ backend/test/server.js
@@ -1,9 +1,10 @@
 const expect  = require("chai").expect;
 const request = require("request");
+const server = require("../server");
 
 describe("MS_Strassenverkehrsamt  API", function() {
     describe("Routes", function() {
-        const RestUrl = "http://localhost:9999" + "/alive";
+        const RestUrl = "http://localhost:8080" + "/alive";
         it("REST Server alive", function() {
             request(RestUrl, function(error, response, body) {
                 expect(response.statusCode).to.equal(200);
